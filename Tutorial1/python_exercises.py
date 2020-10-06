f6e3c7d0388f7d0fafd6fe1b4e18e6a528d4014e
Timothy Ye <timye@umich.edu>
2020-10-06 00:17:43 -0400

    tutorial

--- Tutorial1/python_exercises.py
+++ Tutorial1/python_exercises.py
@@ -3,35 +3,33 @@ Intro to python exercises shell code
 """
 
 def is_odd(x):
-    """
-    returns True if x is odd and False otherwise
-    """
-
+    if x%2 == 1:
+        return True
+    else:
+        return False
 
 def is_palindrome(word):
-    """
-    returns whether `word` is spelled the same forwards and backwards
-    """
+    for i in range(0, int(len(word)/2)):
+        if(word[i] != word[len(word) - 1 -i]):
+            return False
+    return True
 
 
 def only_odds(numlist):
-    """
-    returns a list of numbers that are odd from numlist
-
-    ex: only_odds([1, 2, 3, 4, 5, 6]) -> [1, 3, 5]
-    """
+    new_list = []
+    for i in range(0,len(numlist)):
+        if(numlist[i] %2 == 1):
+            new_list.append(numlist[i])
+    return new_list
 
 
 def count_words(text):
-    """
-    return a dictionary of {word: count} in the text
-
-    words should be split by spaces (and nothing else)
-    words should be converted to all lowercase
-
-    ex: count_words("How much wood would a woodchuck chuck"
-                    " if a woodchuck could chuck wood?")
-        ->
-        {'how': 1, 'much': 1, 'wood': 1, 'would': 1, 'a': 2, 'woodchuck': 2,
-        'chuck': 2, 'if': 1, 'could': 1, 'wood?': 1}
-    """
+    word_list = text.split(' ')
+    d = {}
+    for i in range(0,len(word_list)):
+        count = 0
+        for j in range(0,len(word_list)):
+            if(word_list[i] == word_list[j]):
+                count = count + 1
+        d[word_list[i]] = count
+    return d
